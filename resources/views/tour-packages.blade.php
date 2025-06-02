294259bd478b7ba345205cdc626c390494816710
kaifkhan920 <kaif@taldartech.in>
2025-06-02 11:25:50 +0530

    added images in tour packages

--- resources/views/tour-packages.blade.php
+++ resources/views/tour-packages.blade.php
@@ -98,209 +98,10 @@
                     </div>
                 </div>
                 <!-- Tour Package Cards -->
-                <!-- Havelock Island Getaway -->
-                <div class="col-lg-4 col-md-6 mb-24" data-aos="fade-up" data-aos-duration="1000">
-                    <div class="rx-rooms-main-box">
-                        <div class="rooms-box-front">
-                            <img src="{{ asset('assets/img/tour/havelock.jpg') }}" alt="Havelock Island Tour" style="height: 250px; object-fit: cover;">
-                            <div class="content-wrap">
-                                <div class="inner-contact">
-                                    <h4>Havelock Island Getaway</h4>
-                                    <span class="tour-desc">2 Nights / 3 Days</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="rooms-box-back">
-                            <img src="{{ asset('assets/img/tour/havelock.jpg') }}" alt="Havelock Island Tour">
-                            <div class="content-wrap">
-                                <div class="box-overlay"></div>
-                                <div class="inner-back-side">
-                                    <div class="sub-inner-contact">
-                                        <h5>Havelock Island Getaway</h5>
-                                        <ul>
-                                            <li>Ferry transfers</li>
-                                            <li>Radhanagar Beach visit</li>
-                                            <li>Snorkeling & water sports</li>
-                                            <li>Beach resort stay</li>
-                                            <li>Daily breakfast</li>
-                                        </ul>
-                                        <div class="tour-price">From ₹12,500 per person</div>
-                                        <a href="mailto:kevinsandaman@gmail.com?subject=Tour%20Package%20Inquiry:%20Havelock%20Island" class="tour-inquiry-btn mt-3">Inquire Now</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- North Bay & Ross Island -->
-                <div class="col-lg-4 col-md-6 mb-24" data-aos="fade-up" data-aos-duration="1000">
-                    <div class="rx-rooms-main-box">
-                        <div class="rooms-box-front">
-                            <img src="{{ asset('assets/img/tour/northbay.jpg') }}" alt="North Bay & Ross Island" style="height: 250px; object-fit: cover;">
-                            <div class="content-wrap">
-                                <div class="inner-contact">
-                                    <h4>North Bay & Ross Island</h4>
-                                    <span class="tour-desc">1 Day Excursion</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="rooms-box-back">
-                            <img src="{{ asset('assets/img/tour/northbay.jpg') }}" alt="North Bay & Ross Island">
-                            <div class="content-wrap">
-                                <div class="box-overlay"></div>
-                                <div class="inner-back-side">
-                                    <div class="sub-inner-contact">
-                                        <h5>North Bay & Ross Island</h5>
-                                        <ul>
-                                            <li>Boat transfers</li>
-                                            <li>Coral reef snorkeling</li>
-                                            <li>Historic ruins tour</li>
-                                            <li>Light snacks & water</li>
-                                        </ul>
-                                        <div class="tour-price">From ₹3,500 per person</div>
-                                        <a href="mailto:kevinsandaman@gmail.com?subject=Tour%20Package%20Inquiry:%20North%20Bay%20and%20Ross%20Island" class="tour-inquiry-btn mt-3">Inquire Now</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Port Blair City Tour -->
-                <div class="col-lg-4 col-md-6 mb-24" data-aos="fade-up" data-aos-duration="1000">
-                    <div class="rx-rooms-main-box">
-                        <div class="rooms-box-front">
-                            <img src="{{ asset('assets/img/tour/city.jpg') }}" alt="Port Blair City Tour" style="height: 250px; object-fit: cover;">
-                            <div class="content-wrap">
-                                <div class="inner-contact">
-                                    <h4>Port Blair City Tour</h4>
-                                    <span class="tour-desc">Half / Full Day</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="rooms-box-back">
-                            <img src="{{ asset('assets/img/tour/city.jpg') }}" alt="Port Blair City Tour">
-                            <div class="content-wrap">
-                                <div class="box-overlay"></div>
-                                <div class="inner-back-side">
-                                    <div class="sub-inner-contact">
-                                        <h5>Port Blair City Tour</h5>
-                                        <ul>
-                                            <li>Cellular Jail & Light Show</li>
-                                            <li>Corbyn’s Cove Beach</li>
-                                            <li>Anthropological Museum</li>
-                                            <li>Local shopping stops</li>
-                                        </ul>
-                                        <div class="tour-price">From ₹2,000 per person</div>
-                                        <a href="mailto:kevinsandaman@gmail.com?subject=Tour%20Package%20Inquiry:%20Port%20Blair%20City%20Tour" class="tour-inquiry-btn mt-3">Inquire Now</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Baratang Island Adventure -->
-                <div class="col-lg-4 col-md-6 mb-24" data-aos="fade-up" data-aos-duration="1000">
-                    <div class="rx-rooms-main-box">
-                        <div class="rooms-box-front">
-                            <img src="{{ asset('assets/img/tour/baratang.jpg') }}" alt="Baratang Island Tour" style="height: 250px; object-fit: cover;">
-                            <div class="content-wrap">
-                                <div class="inner-contact">
-                                    <h4>Baratang Island Adventure</h4>
-                                    <span class="tour-desc">1 Day Trip</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="rooms-box-back">
-                            <img src="{{ asset('assets/img/tour/baratang.jpg') }}" alt="Baratang Island Tour">
-                            <div class="content-wrap">
-                                <div class="box-overlay"></div>
-                                <div class="inner-back-side">
-                                    <div class="sub-inner-contact">
-                                        <h5>Baratang Island Adventure</h5>
-                                        <ul>
-                                            <li>Limestone caves</li>
-                                            <li>Mud volcano</li>
-                                            <li>Jarawa reserve drive</li>
-                                            <li>Breakfast & lunch included</li>
-                                        </ul>
-                                        <div class="tour-price">From ₹4,500 per person</div>
-                                        <a href="mailto:kevinsandaman@gmail.com?subject=Tour%20Package%20Inquiry:%20Baratang%20Island" class="tour-inquiry-btn mt-3">Inquire Now</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- 3 Island Special -->
-                <div class="col-lg-4 col-md-6 mb-24" data-aos="fade-up" data-aos-duration="1000">
-                    <div class="rx-rooms-main-box">
-                        <div class="rooms-box-front">
-                            <img src="{{ asset('assets/img/tour/3island.jpg') }}" alt="3 Island Special" style="height: 250px; object-fit: cover;">
-                            <div class="content-wrap">
-                                <div class="inner-contact">
-                                    <h4>3 Island Special</h4>
-                                    <span class="tour-desc">2 Days</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="rooms-box-back">
-                            <img src="{{ asset('assets/img/tour/3island.jpg') }}" alt="3 Island Special">
-                            <div class="content-wrap">
-                                <div class="box-overlay"></div>
-                                <div class="inner-back-side">
-                                    <div class="sub-inner-contact">
-                                        <h5>3 Island Special</h5>
-                                        <ul>
-                                            <li>Havelock, Neil & Ross Islands</li>
-                                            <li>All transfers & sightseeing</li>
-                                            <li>Accommodation included</li>
-                                            <li>Breakfast & select meals</li>
-                                        </ul>
-                                        <div class="tour-price">From ₹16,000 per person</div>
-                                        <a href="mailto:kevinsandaman@gmail.com?subject=Tour%20Package%20Inquiry:%203%20Island%20Special" class="tour-inquiry-btn mt-3">Inquire Now</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Custom Andaman Tour -->
-                <div class="col-lg-4 col-md-6 mb-24" data-aos="fade-up" data-aos-duration="1000">
-                    <div class="rx-rooms-main-box">
-                        <div class="rooms-box-front">
-                            <img src="{{ asset('assets/img/tour/custom.jpg') }}" alt="Custom Andaman Tour" style="height: 250px; object-fit: cover;">
-                            <div class="content-wrap">
-                                <div class="inner-contact">
-                                    <h4>Custom Andaman Tour</h4>
-                                    <span class="tour-desc">Tailored to You</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="rooms-box-back">
-                            <img src="{{ asset('assets/img/tour/custom.jpg') }}" alt="Custom Andaman Tour">
-                            <div class="content-wrap">
-                                <div class="box-overlay"></div>
-                                <div class="inner-back-side">
-                                    <div class="sub-inner-contact">
-                                        <h5>Custom Andaman Tour</h5>
-                                        <ul>
-                                            <li>Personalized itinerary</li>
-                                            <li>Private transfers</li>
-                                            <li>Choice of activities</li>
-                                            <li>Flexible duration & budget</li>
-                                        </ul>
-                                        <div class="tour-price">Contact for quote</div>
-                                        <a href="mailto:kevinsandaman@gmail.com?subject=Tour%20Package%20Inquiry:%20Custom%20Andaman%20Tour" class="tour-inquiry-btn mt-3">Inquire Now</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Package 2 -->
+            
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="tour-package-card">
-                        <img src="{{ asset('assets/img/tour/northbay.jpg') }}" alt="North Bay & Ross Island" class="tour-img">
+                        <img src="{{ asset('assets/img/mount-manipur.jpg') }}" alt="North Bay & Ross Island" class="tour-img">
                         <div class="p-4">
                             <div class="tour-title">North Bay & Ross Island</div>
                             <div class="tour-desc">1 Day Excursion</div>
@@ -310,7 +111,7 @@
                                 <li>Historic ruins tour</li>
                                 <li>Light snacks & water</li>
                             </ul>
-                            <div class="tour-price">From ₹3,500 per person</div>
+                            <div class="tour-price mb-3">From ₹3,500 per person</div>
                             <a href="mailto:kevinsandaman@gmail.com?subject=Tour%20Package%20Inquiry:%20North%20Bay%20and%20Ross%20Island" class="tour-inquiry-btn mt-3">Inquire Now</a>
                         </div>
                     </div>
@@ -318,7 +119,7 @@
                 <!-- Package 3 -->
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="tour-package-card">
-                        <img src="{{ asset('assets/img/tour/city.jpg') }}" alt="Port Blair City Tour" class="tour-img">
+                        <img src="{{ asset('assets/img/mount-manipur.jpg') }}" alt="Port Blair City Tour" class="tour-img">
                         <div class="p-4">
                             <div class="tour-title">Port Blair City Tour</div>
                             <div class="tour-desc">Half / Full Day</div>
@@ -328,7 +129,7 @@
                                 <li>Anthropological Museum</li>
                                 <li>Local shopping stops</li>
                             </ul>
-                            <div class="tour-price">From ₹2,000 per person</div>
+                            <div class="tour-price mb-3git ">From ₹2,000 per person</div>
                             <a href="mailto:kevinsandaman@gmail.com?subject=Tour%20Package%20Inquiry:%20Port%20Blair%20City%20Tour" class="tour-inquiry-btn mt-3">Inquire Now</a>
                         </div>
                     </div>
@@ -336,7 +137,7 @@
                 <!-- Package 4 -->
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="tour-package-card">
-                        <img src="{{ asset('assets/img/tour/baratang.jpg') }}" alt="Baratang Island Tour" class="tour-img">
+                        <img src="{{ asset('assets/img/mount-manipur.jpg') }}" alt="Baratang Island Tour" class="tour-img">
                         <div class="p-4">
                             <div class="tour-title">Baratang Island Adventure</div>
                             <div class="tour-desc">1 Day Trip</div>
@@ -346,7 +147,7 @@
                                 <li>Jarawa reserve drive</li>
                                 <li>Breakfast & lunch included</li>
                             </ul>
-                            <div class="tour-price">From ₹4,500 per person</div>
+                            <div class="tour-price mb-3git ">From ₹4,500 per person</div>
                             <a href="mailto:kevinsandaman@gmail.com?subject=Tour%20Package%20Inquiry:%20Baratang%20Island" class="tour-inquiry-btn mt-3">Inquire Now</a>
                         </div>
                     </div>
@@ -354,7 +155,7 @@
                 <!-- Package 5 -->
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="tour-package-card">
-                        <img src="{{ asset('assets/img/tour/3island.jpg') }}" alt="3 Island Special" class="tour-img">
+                        <img src="{{ asset('assets/img/mount-manipur.jpg') }}" alt="3 Island Special" class="tour-img">
                         <div class="p-4">
                             <div class="tour-title">3 Island Special</div>
                             <div class="tour-desc">2 Days</div>
@@ -364,7 +165,7 @@
                                 <li>Accommodation included</li>
                                 <li>Breakfast & select meals</li>
                             </ul>
-                            <div class="tour-price">From ₹16,000 per person</div>
+                            <div class="tour-price mb-3git ">From ₹16,000 per person</div>
                             <a href="mailto:kevinsandaman@gmail.com?subject=Tour%20Package%20Inquiry:%203%20Island%20Special" class="tour-inquiry-btn mt-3">Inquire Now</a>
                         </div>
                     </div>
@@ -372,7 +173,7 @@
                 <!-- Package 6 -->
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="tour-package-card">
-                        <img src="{{ asset('assets/img/tour/custom.jpg') }}" alt="Custom Andaman Tour" class="tour-img">
+                    <img src="{{ asset('assets/img/mount-manipur.jpg') }}" alt="Custom Andaman Tour" class="tour-img">
                         <div class="p-4">
                             <div class="tour-title">Custom Andaman Tour</div>
                             <div class="tour-desc">Tailored for you</div>
@@ -382,7 +183,7 @@
                                 <li>Expert local guides</li>
                                 <li>Flexible duration & budget</li>
                             </ul>
-                            <div class="tour-price">On Request</div>
+                            <div class="tour-price mb-3git ">On Request</div>
                             <a href="mailto:kevinsandaman@gmail.com?subject=Tour%20Package%20Inquiry:%20Custom%20Andaman%20Tour" class="tour-inquiry-btn mt-3">Inquire Now</a>
                         </div>
                     </div>
